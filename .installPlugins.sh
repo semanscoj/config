82f159ec827426ec6826cda6b4e32529eb7817d5
Jeremee Semansco <jeremee.semansco@gmail.com>
2020-04-22 22:20:53 -0400

    preinstall tmux plugins

--- .installPlugins.sh
+++ .installPlugins.sh
@@ -3,13 +3,14 @@
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 # default vim install is missing +clipboard
-sudo apt-get install vim-gtk
-sudo apt-get install ctags
-sudo apt-get install silversearcher-ag
-sudo apt-get install tmux
-sudo apt-get install tmuxinator
+apt-get install vim-gtk
+apt-get install ctags
+apt-get install silversearcher-ag
+apt-get install tmux
+apt-get install tmuxinator
 
 vim +PluginInstall +qall
+~/.tmux/plugins/tpm/scripts/install_plugins.sh
 
 if [ ! -d "$HOME/.vim/tmp" ]; then
 	mkdir "$HOME/.vim/tmp"
@@ -21,8 +22,8 @@ fi
 
 ln -s ~/.snippets/ultisnips/ ~/.vim/ultisnips
 # Vim You Complete Me Plugin Requirements
-sudo apt-get install cmake python-dev
+apt-get install cmake python-dev
 mkdir /tmp/ycm_build
 cmake -G "Unix Makefiles" /tmp/ycm_build  ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp
 cmake --build /tmp/ycm_build --target ycm_core --config Release
-
+rm -rf BoostParts  CMakeCache.txt  CMakeFiles  Makefile  cmake_install.cmake  compile_commands.json ycm
