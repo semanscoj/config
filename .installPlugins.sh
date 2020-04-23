283fe43441c17f7f18e872b19e6c9c184fba03e5
Jeremee Semansco <jeremee.semansco@gmail.com>
2020-04-22 23:33:34 -0400

    temp remove ycm

--- .installPlugins.sh
+++ .installPlugins.sh
@@ -18,9 +18,3 @@ if [ ! -d "$HOME/.vim/undodir" ]; then
 fi
 
 ln -s ~/.snippets/ultisnips/ ~/.vim/ultisnips
-# Vim You Complete Me Plugin Requirements
-apt-get install cmake python-dev
-mkdir /tmp/ycm_build
-cmake -G "Unix Makefiles" /tmp/ycm_build  ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp
-cmake --build /tmp/ycm_build --target ycm_core --config Release
-rm -rf BoostParts  CMakeCache.txt  CMakeFiles  Makefile  cmake_install.cmake  compile_commands.json ycm
