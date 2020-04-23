d793292b883504cd149fb41fd7e3aa79130264bd
Jeremee Semansco <jeremee.semansco@gmail.com>
2020-04-22 23:46:49 -0400

    move apt install to docker cache

--- .installPlugins.sh
+++ .installPlugins.sh
@@ -2,8 +2,6 @@
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-# default vim install is missing +clipboard
-apt-get install -y vim-gtk ctags silversearcher-ag tmux tmuxinator
 
 vim +PluginInstall +qall
 tmux source ~/.tmux.conf
